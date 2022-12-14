a0f9bd72c21f5d92ec0317f44348438ac33084d6
zojw <robi@hey.com>
2022-12-14 15:55:11 +0800

    add rocksdb args

--- rocks/workload/w_load_seq.sh
+++ rocks/workload/w_load_seq.sh
@@ -3,6 +3,4 @@ source ../config.sh
 keys=25000000
 threads={$1:-1}
 
-THREADS=$threads NUM_KEYS=$keys bash ${ROCKSDB_INSTALL_DIR}/tools/benchmark.sh fillseq_disable_wal &> /tmp/benchsh.txt
-
-grep -A1 "^ops_sec" | tail -1
+env -S "$ENV_VARS" THREADS=$threads NUM_KEYS=$keys bash ${ROCKSDB_INSTALL_DIR}/tools/benchmark.sh fillseq_disable_wal,flush_mt_l0
